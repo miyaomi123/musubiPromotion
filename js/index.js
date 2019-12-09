9a58bdf79a9a77ecf6b3ab528d5f3885be25c423
miyaomi123 <mk08om@gmail.com>
2019-12-09 10:04:39 +0900

    pmx修正

--- js/index.js
+++ js/index.js
@@ -103,7 +103,7 @@ function init() {
   };
     
   /* pmd, vmd File */
-  var modelFile = './mmd/pmd/aichan.pmx'; //エッジ無しverにしないと描画が変になる
+  var modelFile = './mmd/pmd/musubiai2.0.pmx'; //エッジ無しverにしないと描画が変になる
   
   //var vmdFiles = ['./mmd/vmd/地球最後の告白を_Latミクv2.31S_b.vmd'];
 
